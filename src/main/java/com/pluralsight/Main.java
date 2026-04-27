a13abc9c2c7b4b508cddbf7334d81a0c4b220506
andy <andyt.cyb@gmail.com>
2026-04-27 12:05:02 -0400

    added exit prompt for display deposit & payment

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Locale;
 
 public class Main {
@@ -104,17 +105,23 @@ public class Main {
 
 
 
-    //    List transaction test
+
     public static void listAllTransaction(ArrayList<Transaction> transaction) {
 
         while (true) {
+            double spend = 0;
+            transaction.sort(Comparator.comparing(Transaction::getDate).reversed());
+            LocalDate today = LocalDate.now();
             int transactionResult = Integer.parseInt(String.valueOf(transaction.size()));
             PrintFormatUtility.printTransactionHeader();
+
             System.out.printf("You have %d " + ((transactionResult == 1 ? "transaction" : "transactions") + "\n"), transactionResult);
 
             for (Transaction t : transaction) {
                 PrintFormatUtility.formattedTransaction(t);
+                spend += t.getAmount();
             }
+            System.out.printf("Total Spend: $%,.2f", spend);
 
 
             boolean exit = Console.promptForExit("Press to exit", "x");
@@ -210,14 +217,21 @@ public class Main {
         }
     }
 
-//    Display deposits only
+    //    Display deposits only
 
     public static void displayDepositTransaction() {
         for (Transaction t : transactionLedger) {
             if (t.getAmount() > 0) {
                 PrintFormatUtility.formattedTransaction(t);
             }
+
+
+
         }
+        boolean exit = Console.promptForExit("Press to exit", "x");
+        if (exit) {
+            return;
+        };
     }
 
     // Display payment transaction
@@ -228,6 +242,10 @@ public class Main {
                 PrintFormatUtility.formattedTransaction(t);
             }
         }
+        boolean exit = Console.promptForExit("Press to exit", "x");
+        if (exit) {
+            return;
+        };
     }
 
     public static String reportMenuOption() {
@@ -244,6 +262,7 @@ public class Main {
                 3. View Year-To-Date report
                 4. View Previous Year report
                 5. View Searh by Vendor
+                6. Custom Search
                 0. Back - Return to the ledger page.
                 H. Return to home.
                 
@@ -268,6 +287,9 @@ public class Main {
                     break;
                 case "5":
                     filterByVendor();
+                    break;
+                case "6":
+                    filterByCustomSearch();
                     break;
                 case "0":
                     return "LEDGER";
@@ -375,8 +397,6 @@ public class Main {
             String userSearchTerm = Console.askForString("Search for a vendor:");
             boolean vendorExist = false;
 
-
-
             for (Transaction t : transactionLedger)
             {
                 if(t.getVendor().equalsIgnoreCase(userSearchTerm)){
@@ -398,6 +418,44 @@ public class Main {
             return;
         };
     }
+
+    public static void filterByCustomSearch(){
+        String startDate = Console.askForString("Start date (format: mm/dd/yyyy) : ");
+        String endDate = Console.askForString("End Date (format: mm/dd/yyyy): ");
+        String askForDescription = Console.askForString("Description:");
+        String askForVendor = Console.askForString("Vendor: ");
+        double askForAmount = Console.askForDouble("Amount: ");
+
+
+
+
+
+
+        System.out.printf(formatDate(startDate));
+
+
+
+
+        for(Transaction t : transactionLedger){
+
+        }
+
+
+    }
+
+    public static String formatDate(String date){
+        String[] split = date.split("/");
+        String formattedStartDate = split[2] + "-" +
+                split[0] + "-" +
+                split[1];
+        return formattedStartDate;
+    }
+
+
+
+
+
+
 
 
 }
